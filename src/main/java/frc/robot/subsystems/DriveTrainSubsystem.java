f2a6faf133297ccebd5ed8c76a3586ed9be75b9b
pvictorio <patrick.victorio@gmail.com>
2022-02-20 16:17:19 -0500

    Updated vendordeps json to resolve Build issues. Updated DriveTrainSubsystem for Swerve Drive Kinematics objects.

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -12,35 +12,42 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// Swerve Drive imports
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 import frc.robot.Constants;
 
 @SuppressWarnings("unused")
 public class DriveTrainSubsystem extends SubsystemBase {
-  
+
   /**
    * Spark Max Controllers - SwerveDrive Drive Motors (NEO Brushless)
    */
   private CANSparkMax leftFrontSparkMax = new CANSparkMax(Constants.LEFT_FRONT_SPARK_MAX_ID, Constants.BRUSHLESS_MOTOR);
   private CANSparkMax leftBackSparkMax = new CANSparkMax(Constants.LEFT_BACK_SPARK_MAX_ID, Constants.BRUSHLESS_MOTOR);
-  private CANSparkMax rightFrontSparkMax = new CANSparkMax(Constants.RIGHT_FRONT_SPARK_MAX_ID, Constants.BRUSHLESS_MOTOR);
+  private CANSparkMax rightFrontSparkMax = new CANSparkMax(Constants.RIGHT_FRONT_SPARK_MAX_ID,
+      Constants.BRUSHLESS_MOTOR);
   private CANSparkMax rightBackSparkMax = new CANSparkMax(Constants.RIGHT_BACK_SPARK_MAX_ID, Constants.BRUSHLESS_MOTOR);
-   
-  private RelativeEncoder leftFrontEncoder = leftFrontSparkMax.getEncoder();
-  //private RelativeEncoder leftBackEncoder = leftBackSparkMax.getEncoder();
-  //private RelativeEncoder rightFrontEncoder = rightFrontSparkMax.getEncoder();
-  //private RelativeEncoder rightFrontEncoder = rightFrontSparkMax.getEncoder();
-  
+
+  // private RelativeEncoder leftFrontEncoder = leftFrontSparkMax.getEncoder();
+  // private RelativeEncoder leftBackEncoder = leftBackSparkMax.getEncoder();
+  // private RelativeEncoder rightFrontEncoder = rightFrontSparkMax.getEncoder();
+  // private RelativeEncoder rightFrontEncoder = rightFrontSparkMax.getEncoder();
+
   /**
    * TalonSRX Controllers - SwerveDrive PG Steer Motors (PG)
    */
   private WPI_TalonSRX leftFrontTalonSRX = new WPI_TalonSRX(Constants.LEFT_FRONT_TALON_SRX_ID);
-  // private WPI_TalonSRX leftBackTalonSRX = new WPI_TalonSRX(Constants.LEFT_BACK_SPARK_MAX_ID);
+  // private WPI_TalonSRX leftBackTalonSRX = new
+  // WPI_TalonSRX(Constants.LEFT_BACK_SPARK_MAX_ID);
   private WPI_TalonSRX rightFrontTalonSRX = new WPI_TalonSRX(Constants.RIGHT_FRONT_SPARK_MAX_ID);
-  // private WPI_TalonSRX rightBackTalxonSRX = new WPI_TalonSRX(Constants.RIGHT_BACK_SPARK_MAX_ID);
-  
+  // private WPI_TalonSRX rightBackTalxonSRX = new
+  // WPI_TalonSRX(Constants.RIGHT_BACK_SPARK_MAX_ID);
+
   // Lamprey encoder - SwerveDrive
   // DevNote: Figure out Lamprey encoder class name and imports.
   // private RelativeEncoder leftFrontSteeringEncoder = ???.getEncoder();
@@ -48,12 +55,48 @@ public class DriveTrainSubsystem extends SubsystemBase {
   // private RelativeEncoder rightFrontSteeringEncoder = ???.getEncoder();
   // private RelativeEncoder rightBackSteeringEncoder = ???.getEncoder();
 
-  //private XboxController assistantDriverController = new XboxController(Constants.XBOX_ASSISTANT_DRIVER_CONTROLLER_ID);
-  
+  // private XboxController assistantDriverController = new
+  // XboxController(Constants.XBOX_ASSISTANT_DRIVER_CONTROLLER_ID);
+
   /**
-   * Xbox controller object used in the case the driver drives with an Xbox controller
+   * Xbox controller object used in the case the driver drives with an Xbox
+   * controller
    */
   private XboxController driverController = new XboxController(Constants.XBOX_DRIVER_CONTROLLER_PORT_ID);
+
+  /**
+   * SwerveDrive Kinematics
+   */
+  // Locations for the swerve drive modules relative to the robot center.
+  Translation2d m_frontLeftLocation = new Translation2d(Constants.DISTANCE_FROM_CENTER, Constants.DISTANCE_FROM_CENTER);
+  Translation2d m_frontRightLocation = new Translation2d(Constants.DISTANCE_FROM_CENTER,
+      -Constants.DISTANCE_FROM_CENTER);
+  Translation2d m_backLeftLocation = new Translation2d(-Constants.DISTANCE_FROM_CENTER, Constants.DISTANCE_FROM_CENTER);
+  Translation2d m_backRightLocation = new Translation2d(-Constants.DISTANCE_FROM_CENTER,
+      -Constants.DISTANCE_FROM_CENTER);
+  // Creating my kinematics object using the module locations
+  SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+  // Chassis speeds: 1 meter per second forward, 3 meters
+  // per second to the left, and rotation at 1.5 radians per second
+  // counterclockwise.
+  ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);  // DevNote: Fix these params.
+
+  // Convert to module states
+  SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
+
+  // Front left module state
+  SwerveModuleState frontLeft = moduleStates[0];
+
+  // Front right module state
+  SwerveModuleState frontRight = moduleStates[1];
+
+  // Back left module state
+  SwerveModuleState backLeft = moduleStates[2];
+
+  // Back right module state
+  SwerveModuleState backRight = moduleStates[3];
 
   /**
    * Joystick objects used in the case the driver drives with joysticks
@@ -62,13 +105,15 @@ public class DriveTrainSubsystem extends SubsystemBase {
   private Joystick rightJoystick = new Joystick(Constants.DRIVER_JOYSTICK_X_PORT_ID);
 
   /**
-   * Speeds used for arcade drive. Y for forwards and backwards. X for turning left and right
+   * Speeds used for arcade drive. Y for forwards and backwards. X for turning
+   * left and right
    */
   private double yDriveSpeed = 0.0;
   private double xDriveSpeed = 0.0;
 
   /**
-   * Speeds used for tank drive. Left for left side of bot. Right for right side of bot
+   * Speeds used for tank drive. Left for left side of bot. Right for right side
+   * of bot
    */
   private double leftDriveSpeed = 0.0;
   private double rightDriveSpeed = 0.0;
@@ -76,10 +121,9 @@ public class DriveTrainSubsystem extends SubsystemBase {
   /**
    * Creates a new driveTrainSubsystem.
    */
-  public DriveTrainSubsystem() 
-  {
+  public DriveTrainSubsystem() {
     System.out.println("DEVCHECK DriveTrainSubsystem constructor");
-    //Sets left side of Spark Maxs inverted for proper functioning
+    // Sets left side of Spark Maxs inverted for proper functioning
     leftFrontSparkMax.setInverted(true);
     leftBackSparkMax.setInverted(true);
   }
@@ -87,67 +131,67 @@ public class DriveTrainSubsystem extends SubsystemBase {
   /**
    * Method for using Xbox Controllers for arcade drive
    */
-  public void xboxArcadeDrive()
-  {
-    //Sets forwards and backwards speed (y) to the y-axis of the left joystick. Sets turning speed (x) tp x-axis of right joystick
+  public void xboxArcadeDrive() {
+    // Sets forwards and backwards speed (y) to the y-axis of the left joystick.
+    // Sets turning speed (x) tp x-axis of right joystick
     yDriveSpeed = driverController.getLeftY() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
     xDriveSpeed = driverController.getRightX() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
 
-    //System.out.printf("DriveSpeed:  LeftY=%s  RightX=%s \n", xDriveSpeed, yDriveSpeed);
-    //Calls arcade drive method and sends speeds
+    // System.out.printf("DriveSpeed: LeftY=%s RightX=%s \n", xDriveSpeed,
+    // yDriveSpeed);
+    // Calls arcade drive method and sends speeds
     arcadeDrive(yDriveSpeed, xDriveSpeed);
   }
-  
+
   /**
    * Method for using Joysticks for arcade drive
    */
-  public void joystickArcadeDrive()
-  {
-    //Sets forwards and backwards speed (y) to the y-axis of the left joystick. Sets turning speed (x) tp x-axis of right joystick
+  public void joystickArcadeDrive() {
+    // Sets forwards and backwards speed (y) to the y-axis of the left joystick.
+    // Sets turning speed (x) tp x-axis of right joystick
     yDriveSpeed = leftJoystick.getX() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
     xDriveSpeed = rightJoystick.getY() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
 
-    //Calls arcade drive method and sends speeds
+    // Calls arcade drive method and sends speeds
     arcadeDrive(-yDriveSpeed, -xDriveSpeed);
   }
 
   /**
    * Method for using an xbox controller for tank drive
    */
-  public void xboxTankDrive()
-  {
-    //Sets left side of bot speed to the y-axis of the left joystick. Sets right side of bot speed to y-axis of the right joystick
+  public void xboxTankDrive() {
+    // Sets left side of bot speed to the y-axis of the left joystick. Sets right
+    // side of bot speed to y-axis of the right joystick
     leftDriveSpeed = driverController.getLeftY() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
     rightDriveSpeed = driverController.getRightY() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
 
-    //Calls tank drive method and sends speeds
+    // Calls tank drive method and sends speeds
     tankDrive(leftDriveSpeed, rightDriveSpeed);
   }
 
   /**
    * Method for using Joystick controllers for tank drive
    */
-  public void joystickTankDrive()
-  {
-    //Sets left side of bot speed to the y-axis of the left joystick. Sets right side of bot speed to y-axis of the right joystick
+  public void joystickTankDrive() {
+    // Sets left side of bot speed to the y-axis of the left joystick. Sets right
+    // side of bot speed to y-axis of the right joystick
     leftDriveSpeed = leftJoystick.getX() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
     rightDriveSpeed = rightJoystick.getX() * Constants.TELEOP_DRIVE_SPEED_MODIFIER;
 
-    //Calls tank drive method and sends speeds
+    // Calls tank drive method and sends speeds
     tankDrive(leftDriveSpeed, rightDriveSpeed);
   }
 
   /**
    * Method that enables movement via arcade style drive
    */
-  public void arcadeDrive(double ySpeed, double xSpeed)
-  {
-    //Assigns motor to forwards/backwards speed if no turning is detected
+  public void arcadeDrive(double ySpeed, double xSpeed) {
+    // Assigns motor to forwards/backwards speed if no turning is detected
     drive(ySpeed, ySpeed);
-    //If turning is detected, it will be added to one speed side and subtracted from the other speed side to generate the effect of turning
-    //whilst moving forwards/backwards at the same time
-    if (xSpeed >= 0.05 || xSpeed <= -0.05)
-    {
+    // If turning is detected, it will be added to one speed side and subtracted
+    // from the other speed side to generate the effect of turning
+    // whilst moving forwards/backwards at the same time
+    if (xSpeed >= 0.05 || xSpeed <= -0.05) {
       drive(-xSpeed + ySpeed, xSpeed + ySpeed);
     }
   }
@@ -155,27 +199,24 @@ public class DriveTrainSubsystem extends SubsystemBase {
   /**
    * Method that enables movement via tank style drive
    */
-  public void tankDrive(double leftJoySpeed, double rightJoySpeed)
-  {
+  public void tankDrive(double leftJoySpeed, double rightJoySpeed) {
     drive(leftJoySpeed, rightJoySpeed);
   }
 
   /**
    * Assigns speeds to left and right controllers on bot
    */
-  public void drive(double leftSpeed, double rightSpeed)
-  {
-  
-    if(!Constants.isTargeting)
-    {
+  public void drive(double leftSpeed, double rightSpeed) {
+
+    if (!Constants.isTargeting) {
       // Drive motors
       leftFrontSparkMax.set(leftSpeed);
       rightFrontSparkMax.set(rightSpeed);
-      //leftBackSparkMax.set(leftSpeed);
-      //rightBackSparkMax.set(rightSpeed);
+      // leftBackSparkMax.set(leftSpeed);
+      // rightBackSparkMax.set(rightSpeed);
 
       // Steer motors
       rightFrontTalonSRX.set(rightSpeed);
-   }
+    }
   }
 }
